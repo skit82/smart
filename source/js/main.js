cf23c4f969b16fb82af31b4faecd56281ff8388f
Aleksey <a.abrosimow2012@yandex.ru>
2021-07-08 12:20:25 +0300

    Внесение правок в Js и доработки

--- source/js/main.js
+++ source/js/main.js
@@ -9,13 +9,43 @@
   var popupPhone = popupForm.querySelector('[name=callform-tel]');
   var popupText = popupForm.querySelector('[name=callform-text]');
   var pageMask= document.querySelector('.mask');
-  var footerButtons = document.querySelectorAll('.page-footer__button');
+  // var footerButtons = document.querySelectorAll('.page-footer__button');
   var footerLists = document.querySelectorAll('.page-footer__list-hide');
   var body = document.querySelector('body');
-  var footerButtonsArray = Array.prototype.slice.call(footerButtons);
-  var footerListsArray = Array.prototype.slice.call(footerLists);
+  // var footerButtonsArray = Array.prototype.slice.call(footerButtons);
+  // var footerListsArray = Array.prototype.slice.call(footerLists);
   var isStorageSupport = true;
   var storage = "";
+  var accordionItems = document.querySelectorAll('.accordion');
+  var accordionPanes = document.querySelectorAll('.accordion__pane');
+
+  var hidePane = function (button, pane) {
+    button.classList.add('accordion__toggle--inactive');
+    pane.classList.add('accordion__pane--hidden');
+  };
+
+  var showPane = function (button, pane) {
+    button.classList.remove('accordion__toggle--inactive');
+    pane.classList.remove('accordion__pane--hidden');
+  };
+
+  var toggleAccordion = function (evt) {
+    Array.prototype.forEach.call(accordionPanes, function (accordionPane) {
+      var button = accordionPane.closest('.accordion').querySelector('.accordion__toggle');
+      if (button === evt.target && !button.classList.contains('accordion__toggle--inactive') || button !== evt.target) {
+        hidePane(button, accordionPane);
+      } else if (button === evt.target) {
+        showPane(button, accordionPane);
+      }
+    });
+  };
+
+  Array.prototype.forEach.call(accordionItems, function (accordion) {
+    var accordionToggleButton = accordion.querySelector('.accordion__toggle');
+    var accordionPane = accordion.querySelector('.accordion__pane');
+    hidePane(accordionToggleButton, accordionPane);
+    accordionToggleButton.addEventListener('click', toggleAccordion);
+  });
 
   try {
     storage = localStorage.getItem('login');
@@ -35,13 +65,12 @@
     element.classList.toggle(selector);
   };
 
-  if (footerListsArray) {
-    addClass(footerListsArray[FOOTER_BUTTON_NUMBER], 'page-footer__list-disable');
-    removeClass(footerButtonsArray[FOOTER_BUTTON_NUMBER], 'page-footer__button--active');
-  }else if (footerListsArray === FOOTER_BUTTON_NUMBER) {
-      addClass(footerListsArray[FOOTER_BUTTON_NUMBER], 'page-footer__list-disable');
-    removeClass(footerButtonsArray[FOOTER_BUTTON_NUMBER], 'page-footer__button--active');
-  }
+  // if (footerListsArray) {
+    // addClass(footerListsArray[FOOTER_BUTTON_NUMBER], 'page-footer__list-disable');
+    // removeClass(footerButtonsArray[FOOTER_BUTTON_NUMBER], 'page-footer__button--active');
+  // }else {
+    // removeClass(footerListsArray[FOOTER_BUTTON_NUMBER], 'page-footer__list-disable');
+  // }
 
   if (contactButton) {
     contactButton.addEventListener('click', function () {
@@ -91,14 +120,14 @@
     });
   }
 
-  if (footerButtons) {
-    footerButtonsArray.forEach(function (button, i) {
-      button.addEventListener('click', function () {
-        toggleClass(button, 'page-footer__button--active');
-        toggleClass(footerListsArray[i], 'page-footer__list-disable');
-      });
-    });
-  }
+  // if (footerButtons) {
+    // footerButtonsArray.forEach(function (button, i) {
+      // button.addEventListener('click', function () {
+        // toggleClass(button, 'page-footer__button--active');
+        // removeClass(footerListsArray[i], 'page-footer__list-disable');
+      // });
+    // });
+  // }
 
 
   document.querySelectorAll('a[href^="#"]').forEach(anchor => {
